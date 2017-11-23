479d442e93a97bf4f28b8ae0a401e2ad392c405d
Amar <Amar@amartyas-mbp.home>
2017-11-23 08:53:53 -0500

    The game of "Jump It" uses a board of n positive integers in a row except for the very first column (which is always 0). The goal is to move from the first to the last column with the lowest total cost. The number in each column represents the cost to enter that column. The total cost of a game is the sum of the costs of the visited columns. One can only move to the adjacent column or jump and land two columns over.

--- JumpIt_Cost_Recursive/main.cpp
+++ JumpIt_Cost_Recursive/main.cpp
@@ -2,14 +2,45 @@
 //  main.cpp
 //  JumpIt_Cost_Recursive
 //
-//  Created by Amar on 11/23/17.
-//  Copyright © 2017 Amar. All rights reserved.
-//
+
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int costOfJump(int arr[], int ind, int last);
+
+int main(){
+    int arrSize = 6;
+    int last = arrSize-1;   //index of the last element
+    int ind = 0;
+    int gameBoard[6] = {0,3,80,6,5,10};
+
+    int cost = costOfJump(gameBoard, ind, last);
+
+    cout << "The cost is: " << cost << endl;
+    cout << "The cost should be 19." << endl;
+
+
     return 0;
 }
+
+int costOfJump(int arr[], int ind, int last){
+    if(ind==last){          //base case where the array is a single element
+        return arr[ind];
+    }
+    else if(ind+1 == last){ //base case where the array has 2 elements
+        return arr[ind]+costOfJump(arr, ind+1, last);   //will go back to first base case which will return the second element
+    }
+    else if(ind+2 == last){ //base case where the array has 3 elements
+        return arr[ind]+costOfJump(arr, ind+2, last);   //will go back to the first base case which will return the last element
+    }
+    else{
+        if(costOfJump(arr, ind+1, last) <= costOfJump(arr, ind+2, last)){   //down the rabbit hole :)
+            return arr[ind]+costOfJump(arr, ind+1, last);
+        }
+        else{
+            return arr[ind]+costOfJump(arr, ind+2, last);
+        }
+    }
+}
+
