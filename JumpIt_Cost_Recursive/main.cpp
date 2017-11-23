a61a51ddf9db1f40cea915b4f085b3b6dcbe6217
Amar <Amar@amartyas-mbp.home>
2017-11-23 08:37:43 -0500

    Initial Commit

---
+++ JumpIt_Cost_Recursive/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  JumpIt_Cost_Recursive
+//
+//  Created by Amar on 11/23/17.
+//  Copyright © 2017 Amar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
